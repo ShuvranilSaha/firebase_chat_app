8004da80d0c31501a89d9ac816804b99529257b5
ShuvranilSaha <subranil.saha@gmail.com>
2020-09-20 11:24:48 +0530

    first commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, i) => Container(
+          padding: EdgeInsets.all(10),
+          child: Text('This works!'),
+        ),
+      ),
+    );
+  }
+}
