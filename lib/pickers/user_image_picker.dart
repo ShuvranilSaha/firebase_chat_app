949f688de3094af8e06178834ca0e74616149616
ShuvranilSaha <subranil.saha@gmail.com>
2020-09-27 12:16:28 +0530

    added image feature in auth form

---
+++ lib/pickers/user_image_picker.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  Future<void> _imagePicker() async {
+    final imagePicker = ImagePicker();
+    final resultImage = await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = File(resultImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).accentColor,
+          onPressed: _imagePicker,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+        )
+      ],
+    );
+  }
+}
